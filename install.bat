0b27ef564ef6605e8b03cd9f6ee5ef26964076b2
dammitpogi <paul.celebrado@gmail.com>
2020-12-23 13:54:16 -0800

    This is mine. You probably shouldn't use it.

--- install.bat
+++
@@ -1,2 +0,0 @@
-mklink /J "C:\SteamLibrary\steamapps\common\RimWorld\Mods" "C:\SteamLibrary\steamapps\common\RimWorld\GitHub\Hardcore-SK\Mods"
-mklink /J "C:\Users\Siren\AppData\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Config" "C:\SteamLibrary\steamapps\common\RimWorld\GitHub\Hardcore-SK\Config"

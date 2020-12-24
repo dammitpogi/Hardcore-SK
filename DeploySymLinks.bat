bd4bd4aef050d79959512f71bd5b762f66fc8247
dammitpogi <paul.celebrado@gmail.com>
2020-12-23 23:55:59 -0800

    Update DeploySymLinks.bat

--- DeploySymLinks.bat
+++ DeploySymLinks.bat
@@ -1,2 +1,5 @@
+:: This is mine. You probably shouldn't use it unless you update it first. And no. I'm not gonna hold your hand. But I will give you a reference.
+:: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/mklink
+
 mklink /J "C:\SteamLibrary\steamapps\common\RimWorld\Mods" "C:\SteamLibrary\steamapps\common\RimWorld\GitHub\Hardcore-SK\Mods"
 mklink /J "C:\Users\Siren\AppData\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Config" "C:\SteamLibrary\steamapps\common\RimWorld\GitHub\Hardcore-SK\Config"
